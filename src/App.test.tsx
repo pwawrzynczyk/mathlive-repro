c28fa9e9556c90c0a4a1dd056018026f56b9a18e
Piotr Wawrzyńczyk <piotr.wawrzynczyk@omnicalculator.com>
2024-01-29 12:19:24 +0100

    comments

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('test', () => {
+  it('renders learn react link', async () => {
+    const { getByTestId } = render(<App />);
+
+    userEvent.type(getByTestId('test-id'), '10 + 2')
+
+    // fireEvent.input(getByTestId('test-id'), {
+    //   target: {value: '10+2'}
+    // })
+
+    await waitFor(() => {
+      expect(getByTestId('result')).toHaveTextContent('Value: 10 + 2')
+    })
+  });
+})
